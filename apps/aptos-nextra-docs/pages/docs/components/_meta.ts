820c84a933aa62cbb68ec71d23569975e430c55e
Andrew Hariri <hariria@usc.edu>
2024-01-24 13:54:11 -0800

    [aptos-nextra-docs] Instantiate new project and docs site

---
+++ apps/aptos-nextra-docs/pages/docs/components/_meta.ts
@@ -0,0 +1,9 @@
+export default {
+  mermaid: 'Mermaid',
+  npm2yarn: 'Npm2Yarn',
+  'tailwind-css': 'Tailwind CSS',
+  latex: 'LaTeX',
+  table: 'Rendering Tables',
+  typescript: '',
+  remote: 'Remote Content',
+}
---
+++ packages/aptos-nextra-theme-docs/src/components/back-to-top.tsx
@@ -0,0 +1,38 @@
+import cn from 'clsx'
+import { ArrowRightIcon } from 'nextra/icons'
+import type { ReactElement } from 'react'
+import { useEffect, useRef } from 'react'
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+export function BackToTop({ className }: { className?: string }): ReactElement {
+  const ref = useRef<HTMLButtonElement>(null)
+  useEffect(() => {
+    function toggleVisible() {
+      const { scrollTop } = document.documentElement
+      ref.current?.classList.toggle('_opacity-0', scrollTop < 300)
+    }
+
+    window.addEventListener('scroll', toggleVisible)
+    return () => {
+      window.removeEventListener('scroll', toggleVisible)
+    }
+  }, [])
+
+  return (
+    <button
+      ref={ref}
+      aria-hidden="true"
+      onClick={scrollToTop}
+      className={cn(
+        '_flex _items-center _gap-1.5 _transition _opacity-0',
+        className
+      )}
+    >
+      Scroll to top
+      <ArrowRightIcon className="_-rotate-90 _w-3.5 _h-3.5 _border _rounded-full _border-current" />
+    </button>
+  )
+}
