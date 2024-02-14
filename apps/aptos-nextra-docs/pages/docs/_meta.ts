864cf0bc8d02af6e27ddfafe44e21fbb97c24459
Andrew Hariri <hariria@usc.edu>
2024-02-13 22:48:49 -0800

    [aptos-nextra-docs] Remove aptos-nextra-docs

--- apps/aptos-nextra-docs/pages/docs/_meta.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  index: 'Welcome',
-  setup: 'Setup',
-  components: "Components",
-  "docs-theme": "Docs Theme",
-  "blog-theme": "Blog theme",
-  examples: "Examples",
-}
--- apps/aptos-nextra-docs/pages/docs/setup/_meta.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  'get-started': 'Get started',
-  'maintenance': "Maintenance",
-}
