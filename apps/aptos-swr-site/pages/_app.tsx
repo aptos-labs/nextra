f379c9ca0206f1b94d963b8dcdf6ac094abaa92b
Andrew Hariri <hariria@usc.edu>
2024-02-19 19:22:22 -0800

    [aptos-swr-site] Google Analytics

--- apps/aptos-swr-site/pages/_app.tsx
+++ apps/aptos-swr-site/pages/_app.tsx
@@ -1,5 +1,16 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
 import '../styles.css'
 
+/**
+ * Learn more about using Google Analytics 4 with the 
+ * Next.js pages router here
+ * @see https://nextjs.org/docs/pages/building-your-application/optimizing/third-party-libraries#google-analytics
+ */
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics gaId="G-LLF79THJN0" />
+      <Component {...pageProps} />
+    </>
+  )
 }
