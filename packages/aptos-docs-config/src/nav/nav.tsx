fc111e80309631ecb0e0d4a1a89286fda319e7bc
Andrew Hariri <hariria@usc.edu>
2024-02-28 21:09:01 -0800

    [aptos-swr-site] navbar

--- packages/aptos-docs-config/src/nav/nav.tsx
+++ packages/aptos-docs-config/src/nav/nav.tsx
@@ -1,83 +1,77 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
-import type { ReactElement } from 'react';
 
-export interface NavigationMenuItemType {
-  title: string
-  subtitle: string
-  link: string
-  iconUrl?: string
-  iconSvg?: ReactElement
-}
+type Page = {
+  title: string;
+  type: 'page';
+  display?: 'hidden';
+  href?: string;
+  newWindow?: boolean;
+  theme?: {
+    layout?: 'full';
+    sidebar?: boolean;
+    toc?: boolean;
+    breadcrumb?: boolean;
+    typesetting?: 'article';
+  };
+};
 
-export interface NavigationMenuSectionType {
-  title: string
-  items: NavigationMenuItemType[]
-}
+type Menu = {
+  title: string;
+  type: 'menu';
+  items: {
+    [key: string]: {
+      title: string;
+      href?: string;
+      newWindow?: boolean;
+    };
+  };
+};
 
-export interface NavigationMenuType {
-  sections: NavigationMenuSectionType[]
-}
+type ConfigItem = Page | Menu;
+
+type Config = {
+  [key: string]: ConfigItem;
+};
 
 export enum NavigationHeaderCategories {
-  DEVELOPERS = 'developers',
-  PRODUCTS = 'products',
+  BUILD = 'build',
+  DEVELOPER_PLATFORMS = 'developer_platforms',
   NETWORK = 'network'
 }
 
-export type NavigationHeaderType = Record<
-  NavigationHeaderCategories,
-  NavigationMenuType
->
-
-export const defaultNav: NavigationHeaderType = {
-  developers: {
-    sections: [
+export const defaultNav: Config = {
+  build: {
+    items: [
       {
-        title: 'Developer Tooling',
-        items: [
-          {
-            title: 'Typescript SDK',
-            subtitle: 'Build web applications',
-            iconUrl:
-              'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
-            link: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'
-          },
-          {
-            title: 'Typescript SDK',
-            subtitle: 'Build web applications',
-            iconUrl:
-              'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
-            link: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'
-          }
-        ]
+        title: 'Typescript SDK',
+        subtitle: 'Build web applications',
+        iconUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+        link: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'
       },
       {
-        title: 'Move',
-        items: [
-          {
-            title: 'Typescript SDK',
-            subtitle: 'Build web applications',
-            iconUrl:
-              'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
-            iconSvg: <div>hi</div>,
-            link: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'
-          },
-          {
-            title: 'Typescript SDK',
-            subtitle: 'Build web applications',
-            iconUrl:
-              'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
-            link: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'
-          }
-        ]
+        title: 'Typescript SDK',
+        subtitle: 'Build web applications',
+        iconUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+        link: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'
+      },
+      {
+        title: 'Typescript SDK',
+        subtitle: 'Build web applications',
+        iconUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+        iconSvg: <div>hi</div>,
+        link: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'
+      },
+      {
+        title: 'Typescript SDK',
+        subtitle: 'Build web applications',
+        iconUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+        link: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg'
       }
     ]
   },
-  network: {
-    sections: []
-  },
-  products: {
-    sections: []
-  }
 }
